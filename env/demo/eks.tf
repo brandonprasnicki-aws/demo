95f9baa5e60e4c4aeaea1125473f74b9060012bf
Brandon Prasnicki <Brandon.Prasnicki@tekstream.com>
2026-01-11 19:55:33 -0600

    node group

--- env/demo/eks.tf
+++ env/demo/eks.tf
@@ -3,4 +3,6 @@ module "eks_cluster" {
   cluster_name = "my-cluster"
   subnet_ids   = [module.VPC.pri_subnets_az1_ids[0], module.VPC.pri_subnets_az2_ids[0]]
   access_entry_principal_arn = "arn:aws:iam::042776340228:user/brandonprasnicki@gmail.com"
+  node_group_name = "my-node-group"
+  ssh_key_name = "mysshkey"
 }
