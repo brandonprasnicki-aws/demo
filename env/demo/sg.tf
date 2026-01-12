748e632bf8f4fa5ff3eab0fa9aaa5a6d028e2d74
Brandon Prasnicki <Brandon.Prasnicki@tekstream.com>
2026-01-11 18:51:59 -0600

    clean up changes

--- env/demo/sg.tf
+++ env/demo/sg.tf
@@ -1,21 +1,13 @@
 /*
 module "_LBSecurityGroup" {
-<<<<<<< HEAD
-  source         = "../../modules/modules/SecurityGroup"
-=======
-  source         = "\.\./\.\./modules/SecurityGroup"
->>>>>>> fcdad151850847e8cf8e830aeeedc7711331988e
+  source         = "../../modules/SecurityGroup"
   common_tags    = var.common_tags
   sg_description = "lb sg"
   sg_name        = "-lb-sg"
   vpc_id         = module.IngressVPC.vpc_id
 }
 module "_LB_Cidr_Rule_443" {
-<<<<<<< HEAD
   source                = "../../modules/modules/SecurityGroupCidrRules"
-=======
-  source                = "\.\./\.\./modules/SecurityGroupCidrRules"
->>>>>>> fcdad151850847e8cf8e830aeeedc7711331988e
   allowed_ingress_cidrs = ["0.0.0.0/0"] #var.on_prem_cidrs * will be restricted cidr var prob
   from_port             = "443"
   to_port               = "443"
@@ -24,11 +16,7 @@ module "_LB_Cidr_Rule_443" {
 }
 
 module "_LB_Cidr_Rule_80" {
-<<<<<<< HEAD
   source                = "../../modules/modules/SecurityGroupCidrRules"
-=======
-  source                = "\.\./\.\./modules/SecurityGroupCidrRules"
->>>>>>> fcdad151850847e8cf8e830aeeedc7711331988e
   allowed_ingress_cidrs = ["0.0.0.0/0"] #var.on_prem_cidrs * will be restricted cidr var prob
   from_port             = "80"
   to_port               = "80"
